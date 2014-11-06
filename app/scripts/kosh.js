4ce79214e48fdd144cea395466b8f50ca8c4e33a
Philipp Nowinski <philipp@nowinski.de>
2014-11-06 16:51:21 +0100

    introducing routes

--- app/scripts/kosh.js
+++ app/scripts/kosh.js
@@ -1,5 +1,16 @@
 'use strict';
 
-var app = angular.module('kosh', ['firebase']);
+var app = angular.module('kosh', ['firebase', 'ngRoute']);
+
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'templates/tickets.html',
+      controller: 'MainCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+});
 
 app.constant('FIREBASE_URI', 'https://sizzling-inferno-7416.firebaseio.com/');
