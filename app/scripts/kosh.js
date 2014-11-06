133c9302dd5d882bc924a4380aa47897e0b0e0e7
Philipp Nowinski <philipp@nowinski.de>
2014-11-06 16:25:30 +0100

    fetch tickets from firebase

--- app/scripts/kosh.js
+++ app/scripts/kosh.js
@@ -1,3 +1,5 @@
 'use strict';
 
 angular.module('kosh', ['firebase']);
+
+angular.module('kosh').constant('FIREBASE_URI', 'https://sizzling-inferno-7416.firebaseio.com/');
--- app/scripts/main/main-ctrl.js
+++ app/scripts/main/main-ctrl.js
@@ -3,63 +3,38 @@
 'use strict';
 
 angular.module('kosh')
-  .controller('MainCtrl', function ($scope, $firebase) {
-  	// now we can use $firebase to synchronize data between clients and the server!
-    var ref = new Firebase('https://sizzling-inferno-7416.firebaseio.com/');
-    var sync = $firebase(ref);
+  .controller('MainCtrl', function ($scope, TicketService) {
 
-    $scope.awesomeThings = [
-      {
-        'key': 'angular',
-        'title': 'AngularJS',
-        'url': 'https://angularjs.org/',
-        'description': 'HTML enhanced for web apps!',
-        'logo': 'angular.png'
-      },
-      {
-        'key': 'browsersync',
-        'title': 'BrowserSync',
-        'url': 'http://browsersync.io/',
-        'description': 'Time-saving synchronised browser testing.',
-        'logo': 'browsersync.png'
-      },
-      {
-        'key': 'gulp',
-        'title': 'GulpJS',
-        'url': 'http://gulpjs.com/',
-        'description': 'The streaming build system.',
-        'logo': 'gulp.png'
-      },
-      {
-        'key': 'jasmine',
-        'title': 'Jasmine',
-        'url': 'http://jasmine.github.io/',
-        'description': 'Behavior-Driven JavaScript.',
-        'logo': 'jasmine.png'
-      },
-      {
-        'key': 'karma',
-        'title': 'Karma',
-        'url': 'http://karma-runner.github.io/',
-        'description': 'Spectacular Test Runner for JavaScript.',
-        'logo': 'karma.png'
-      },
-      {
-        'key': 'protractor',
-        'title': 'Protractor',
-        'url': 'https://github.com/angular/protractor',
-        'description': 'End to end test framework for AngularJS applications built on top of WebDriverJS.',
-        'logo': 'protractor.png'
-      },
-      {
-        'key': 'bootstrap',
-        'title': 'Bootstrap',
-        'url': 'http://getbootstrap.com/',
-        'description': 'Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile first projects on the web.',
-        'logo': 'bootstrap.png'
-      }
-    ];
-    angular.forEach($scope.awesomeThings, function(awesomeThing) {
-      awesomeThing.rank = Math.random();
-    });
+    $scope.items = TicketService.getTickets();
+
+    $scope.addItem = function() {
+      TicketService.addTicket($scope.newItem);
+      $scope.newItem = {};
+    };
+
+    $scope.removeItem = function(id) {
+      TicketService.removeTicket(id);
+    };
+
+  });
+
+angular.module('kosh')
+  .factory('TicketService', function($firebase, FIREBASE_URI){
+    var exports = {},
+        ref = new Firebase(FIREBASE_URI + 'tickets'),
+        sync = $firebase(ref);
+
+    exports.getTickets = function() {
+      return sync.$asObject();
+    };
+
+    exports.removeTicket = function(id) {
+      sync.$remove(id);
+    };
+
+    exports.addTicket = function(ticket) {
+      sync.$push(ticket);
+    };
+
+    return exports;
   });
