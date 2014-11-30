c3e3c5111364f3b4ecf2e1cc9f4948c51043b5ae
Philipp Nowinski <philipp@nowinski.de>
2014-11-30 22:12:51 +0100

    first bunch of unit tests for ticket-controller

---
+++ app/modules/ticket/test/unit/ticket-controller-test.js
@@ -0,0 +1,57 @@
+'use strict';
+
+describe('ticket-controller', function () {
+  var scope,
+      TicketServiceMock;
+
+  beforeEach(module('kosh'));
+
+  beforeEach(inject(function($rootScope) {
+    scope = $rootScope.$new();
+  }));
+
+  describe('basics', function () {
+    
+    beforeEach(inject(function($controller) {
+      $controller('TicketController', {
+        $scope: scope
+      });
+    }));
+
+    it('should provide a blank newTicket object', function () {
+      expect(scope.newTicket).toBeDefined();
+    });
+
+    it('should provide a set of priorities', function() {
+      expect(scope.priorities).toBeDefined();
+    });
+
+  });
+
+  describe('single ticket state', function () {
+    beforeEach(inject(function($controller) {
+
+      TicketServiceMock = {
+        get: function() {
+          return {
+            $bindTo: function(){}
+          };
+        }
+      };
+
+      spyOn(TicketServiceMock, 'get').andCallThrough();
+
+      $controller('TicketController', {
+        $scope: scope,
+        $routeParams: {ticketId: 1},
+        TicketService: TicketServiceMock
+      });
+
+    }));
+
+    it('should fetch single ticket if ticketId is passed', function () {
+      expect(TicketServiceMock.get).toHaveBeenCalled();
+    });
+
+  });
+});
